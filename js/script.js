fd452d1c67cd3c0a7998402f02137c5298e9dd1d
biancapcr <biancacristina.pocora@gmail.com>
2025-07-01 16:27:08 +0200

    [add] calc prezzo base

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 // 1) Inserimento dati
 const km = parseFloat(prompt("Quanti chilometri vuoi percorrere?"));
 const age = parseInt(prompt("Quanti anni hai?"), 10);
+
+// 2) Calcolo prezzo base
+const pricePerKm = 0.21;
+let totalPrice = km * pricePerKm;
