8777911c66130baa1f4fe6632b01ad6af7ecb3ec
biancapcr <biancacristina.pocora@gmail.com>
2025-07-01 16:36:24 +0200

    [add] calcolo sconto

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ const age = parseInt(prompt("Quanti anni hai?"), 10);
 // 2) Calcolo prezzo base
 const pricePerKm = 0.21;
 let totalPrice = km * pricePerKm;
+
+// 3) Calcolo sconto
+let discount = 0;
+if (age < 18){
+  discount = 0.20;
+} 
+else if (age > 65){
+  discount = 0.40;
+}
